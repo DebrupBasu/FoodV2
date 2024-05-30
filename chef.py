5d7c436d0bc2323f0d2a021237502112064324ad
Debrup Basu <63699389+DebrupBasu@users.noreply.github.com>
2024-05-30 10:32:14 -0400

    Update chef.py

--- chef.py
+++ chef.py
@@ -13,12 +13,12 @@ if cuisine_choice in ["", "Asian"] and meal_choice in ["Lunch", "Dinner"] and fl
     st.image("https://savoryspin.com/wp-content/uploads/2021/04/PlantBased-Cashew-Curry.jpg", caption="Szechuan Tofu Stir Fry")
     st.write("**Cookbooks to Buy:** [Doug McNish's Cookbooks](https://www.amazon.com/stores/author/B00E5FEE5S/allbooks?ingress=0&visitId=131f99ec-8628-4c07-85cd-4f4dd8d2ff1d)")
 
-if cuisine_choice in ["", "Italian"] and meal_choice in ["Lunch", "Dinner"]:
+if cuisine_choice in ["", "Italian"] and meal_choice in ["Lunch", "Dinner"] and flavor_profile in ['Savory', 'Herbal']:
     st.subheader("**Recipe:** Vegan Mushroom Risotto")
     st.image("https://veganhuggs.com/wp-content/uploads/2018/02/mushroom-risotto-featured.jpg", caption="Vegan Mushroom Risotto")
     st.write("**Cookbooks to Buy:** [Doug McNish's Cookbooks](https://www.amazon.com/stores/author/B00E5FEE5S/allbooks?ingress=0&visitId=131f99ec-8628-4c07-85cd-4f4dd8d2ff1d)")
 
-if cuisine_choice in ["", "Mexican"] and meal_choice in ["Lunch", "Dinner"]:
+if cuisine_choice in ["", "Mexican"] and meal_choice in ["Lunch", "Dinner"] and flavor_profile in ['Savory', 'Spicy']:
     st.subheader("**Recipe:** Vegan Enchiladas with Cashew Cheese")
     st.image("https://i2.wp.com/www.evolvingtable.com/wp-content/uploads/2019/01/Black-Bean-Spinach-Enchiladas_1.jpg", caption="Vegan Enchiladas")
     st.write("**Cookbooks to Buy:** [Doug McNish's Cookbooks](https://www.amazon.com/stores/author/B00E5FEE5S/allbooks?ingress=0&visitId=131f99ec-8628-4c07-85cd-4f4dd8d2ff1d)")
